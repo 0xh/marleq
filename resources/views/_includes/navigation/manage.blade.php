e38634538b6f76c237dfec04d039a5ce2b0e5967
Srdjan Tomovic <srdjan.tomovic@gmail.com>
2018-04-22 23:37:51 +0200

    Initial Commit - Laravel + Laratrust + User CRUD

---
+++ resources/views/_includes/navigation/manage.blade.php
@@ -0,0 +1,18 @@
+<div class="side-menu">
+    <aside class="menu">
+        <p class="menu-label">
+            General
+        </p>
+        <ul class="menu-list">
+            <li><a href="{{route('manage.dashboard')}}">Dashboard</a></li>
+        </ul>
+        <p class="menu-label">
+            Users
+        </p>
+        <ul class="menu-list">
+            <li><a href="{{route('users.index')}}">Manage Users</a></li>
+            <li><a href="{{route('roles.index')}}">Roles</a></li>
+            <li><a href="{{route('permissions.index')}}">Permissions</a></li>
+        </ul>
+    </aside>
+</div>
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::group(['prefix' => '/manage', 'middleware' => 'role:superadministrator|administrator'], function () {
+    Route::get('/', ['as' => 'manage', 'uses' => 'ManageController@index']);
+    Route::get('/dashboard', ['as' => 'manage.dashboard', 'uses' => 'ManageController@dashboard']);
+    Route::resource('/users', 'UserController');
+    Route::resource('/roles', 'RoleController', ['except' => 'destroy']);
+    Route::resource('/permissions', 'PermissionController', ['except' => 'destroy']);
+});
+Route::get('/home', 'HomeController@index')->name('home');
