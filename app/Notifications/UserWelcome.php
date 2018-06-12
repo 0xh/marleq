3ea26134d67cb0fcf073083716b1d57f42495a30
Srdjan Tomovic <srdjan.tomovic@gmail.com>
2018-06-13 01:21:46 +0200

    Add email notifications for Coaches and Country Managers; Change og-image

--- app/Notifications/UserWelcome.php
+++ app/Notifications/UserWelcome.php
@@ -43,14 +43,40 @@ class UserWelcome extends Notification
      */
     public function toMail($notifiable)
     {
-        return (new MailMessage)
-                    ->subject('Welcome to MARLEQ!')
-                    ->line($this->user->name . ' we\'re delighted to have you on board!')
-                    ->line('You have joined hundreds of job seekers worldwide who use and love MARLEQ.')
-                    ->action('Find your Coach', url('/find-a-coach'))
-                    ->line('If you have any questions regarding your account, please contact us at 
+        if($this->user->hasRole('user')) {
+            return (new MailMessage)
+                ->subject('Welcome to MARLEQ!')
+                ->line($this->user->name . ' we\'re delighted to have you on board!')
+                ->line('You have joined hundreds of job seekers worldwide who use and love MARLEQ.')
+                ->action('Find your Coach', url('/find-a-coach'))
+                ->line('If you have any questions regarding your account, please contact us at 
                     support@marleq.com. Our technical support team will assist you with anything you need.')
-                    ->line('Best of luck with your job search and career progress, and welcome!');
+                ->line('Best of luck with your job search and career progress, and welcome!');
+        }
+
+        if($this->user->hasRole('coach')) {
+            return (new MailMessage)
+                ->subject('Welcome to MARLEQ!')
+                ->line($this->user->name . ' we\'re delighted to have you on board!')
+                ->line('﻿Being a career coach on MARLEQ allows you to have more international impact and clients.')
+                ->line('Start by updating your profile information and after you have completed your profile one of our country managers will contact you.')
+                ->action('Update your profile', url('/user/profile/' . $this->user->alias . '/edit'))
+                ->line('If you have any questions regarding your account, please contact us at 
+                    support@marleq.com. Our technical support team will assist you with anything you need.')
+                ->line('Best of luck with your client acquisition and welcome!');
+        }
+
+        if($this->user->hasRole('country-manager')) {
+            return (new MailMessage)
+                ->subject('Welcome to MARLEQ!')
+                ->line($this->user->name . ' we\'re delighted to have you on board!')
+                ->line('You have joined a worldwide group of career leaders and enthusiasts.')
+                ->line('Start by updating your profile information and after you have completed your profile one of our country managers will contact you.')
+                ->action('Update your profile', url('/user/profile/' . $this->user->alias . '/edit'))
+                ->line('If you have any questions regarding your account, please contact us at 
+                    support@marleq.com. Our technical support team will assist you with anything you need.')
+                ->line('Best of luck with your activities and welcome!');
+        }
     }
 
     /**
